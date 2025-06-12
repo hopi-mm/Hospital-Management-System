87f595db6bca5a9e79216c6e6feb5d8c1ce3ccdf
Moe-Wai-Yan <moorewaiyan@gmail.com>
2025-06-12 22:05:28 +0630

    first commit

---
+++ database/migrations/2025_06_12_042843_create_patients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('relation')->nullable();
+            $table->enum('gender',['male','female'])->nullable();
+            $table->date('dob')->nullable();
+            $table->enum('blood_type',['A+','A-','B+','B-','AB+','AB-','O+','O-'])->nullable();
+            $table->integer('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
---
+++ database/migrations/2025_06_12_043101_create_lab_results_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lab_results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('appointment_id')->constrained()->onDelete('cascade');
+            $table->string('test_name');
+            $table->text('result_summary');
+            $table->text('detailed_result');
+            $table->date('performed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lab_results');
+    }
+};
