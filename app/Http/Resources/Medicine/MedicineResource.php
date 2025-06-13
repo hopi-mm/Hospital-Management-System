0f3b441dcf11b99d2711febb4788c1d282763182
Moe-Wai-Yan <moorewaiyan@gmail.com>
2025-06-13 20:33:06 +0630

    add resource in medicine

--- app/Http/Resources/Medicine/MedicineResource.php
+++ app/Http/Resources/Medicine/MedicineResource.php
@@ -14,6 +14,14 @@ class MedicineResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'description'=>$this->description,
+            'dosage'=>$this->dosage,
+            'unit'=>$this->unit,
+            'price'=>$this->price,
+            'stock'=>$this->stock,
+        ];
     }
 }
