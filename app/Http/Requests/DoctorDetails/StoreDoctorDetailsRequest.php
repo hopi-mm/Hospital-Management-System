b654bcd7e3dc751ee191474b1261331e5bdf8ae2
ThanPhy0 <than.twp972@gmail.com>
2025-06-18 18:18:07 +0700

    request

--- app/Http/Requests/DoctorDetails/StoreDoctorDetailsRequest.php
+++ app/Http/Requests/DoctorDetails/StoreDoctorDetailsRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreDoctorDetailsRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,7 +14,17 @@ class StoreDoctorDetailsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'speciality' => 'required|string|max:255',
+            'experience' => 'required',
+            'education' => 'required|string|max:255',
+            'license_number' => 'required|string|max:255',
+            'availability' => 'array',
+            'availability.Mon' => 'array',
+            'availability.Tue' => 'array',
+            'availability.Wed' => 'array',
+            'availability.Thu' => 'array',
+            'availability.Fri' => 'array',
+            'availability.*.*' => 'string|regex:/^\d{2}:\d{2}$/'
         ];
     }
 }
