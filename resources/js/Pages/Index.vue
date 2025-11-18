caed0b9d0c300ab9f65ee63a2e11f484e1f5f2f6
Phone Htut Khaung <phonehtut@innovix-solutions.com>
2025-11-18 10:56:32 +0630

    Migrate Tailwind v3 to v4 and add shadcn ui

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,9 +1,14 @@
-<script setup>
+<script setup lang="ts">
+
+import {Button} from "@/components/ui/button";
+import { Input } from '@/components/ui/input'
 
 </script>
 
 <template>
     <h1>Hello World</h1>
+    <Button style="margin-left: 20px" variant="default">Button</Button>
+    <Input />
 </template>
 
 <style scoped>
